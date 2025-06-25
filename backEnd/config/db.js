918fd34d35e9f0c80c685b5b5042960162cfaad2
Harish Macha <harishmacha987@gmail.com>
2025-06-25 16:17:35 +0000

    db .js updated

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDb = async () =>{
     try {
-        const conn = await mongoose.connect("mongodb+srv://harishmacha987:LbQNkHBKXlMO2p69@cluster0.2dwxbhk.mongodb.net/foodC");
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB connected : ${conn.connection.host}`);
     } catch (error) {
         console.log(`MongoDB connection error`,error);
