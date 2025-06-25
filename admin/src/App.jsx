ec0143fd1decf9bf5e59d9536f4d1cac450b0bc8
Harish-987 <harishmacha987@gmail.com>
2025-06-25 11:19:59 +0000

    Update App.jsx updated with url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = 'http://localhost:4000';
+  const url = 'https://foodc.onrender.com';
   return (
     <div>
       <ToastContainer />
